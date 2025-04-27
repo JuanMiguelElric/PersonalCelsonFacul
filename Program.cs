3f39a09537e863f204675878d2002bf8cdfc211f
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-04-27 15:25:54 -0300

    as

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PersonalTrainer.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,15 +11,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Adiciona Identity
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Método para criar roles ao iniciar
-CreateRoles(app.Services);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -45,21 +42,3 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
-
-// Método para criar roles se elas não existirem
-void CreateRoles(IServiceProvider serviceProvider)
-{
-    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    string[] roleNames = { "Administrador", "Personal", "Aluno" };
-
-    foreach (var roleName in roleNames)
-    {
-        var roleExist = roleManager.RoleExistsAsync(roleName).Result;
-        if (!roleExist)
-        {
-            var role = new IdentityRole(roleName);
-            var result = roleManager.CreateAsync(role).Result;
-        }
-    }
-}
